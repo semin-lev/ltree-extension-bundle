8bddd79ea94f1143696d7a8297ec25ed3edb3e2d
Lev Semin <lev@darvin-studio.ru>
2015-03-01 11:12:28 +0000

    initial bundle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Slev\LtreeExtensionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SlevLtreeExtensionBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ SlevLtreeExtensionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Slev\LtreeExtensionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SlevLtreeExtensionBundle extends Bundle
+{
+}
