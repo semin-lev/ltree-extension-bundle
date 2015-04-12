d052f337e542c4618a8d0e8b0350dacbd5850936
Lev Semin <lev@darvin-studio.ru>
2015-04-12 12:26:46 +0300

    move config to di extension

--- DependencyInjection/SlevLtreeExtensionExtension.php
+++ DependencyInjection/SlevLtreeExtensionExtension.php
@@ -4,6 +4,7 @@ namespace Slev\LtreeExtensionBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,8 +13,44 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SlevLtreeExtensionExtension extends Extension
+class SlevLtreeExtensionExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $dbalConfig = [
+            'dbal' => [
+                'types' => [
+                    'ltree' => 'Slev\LtreeExtensionBundle\Types\LTreeType'
+                ],
+                'mapping_types' => [
+                    'ltree' => 'ltree'
+                ],
+            ],
+            'orm' => [
+                'repository_factory'=>'slev_ltreeextensionbundle.repository_factory',
+                'dql' => [
+                    'string_functions' => [
+                        'ltree_concat' => 'Slev\LtreeExtensionBundle\DqlFunction\LtreeConcatFunction',
+                        'ltree_subpath' => 'Slev\LtreeExtensionBundle\DqlFunction\LtreeSubpathFunction'
+                    ],
+                    'numeric_functions' => [
+                        'ltree_nlevel' => 'Slev\LtreeExtensionBundle\DqlFunction\LtreeNlevelFunction',
+                        'ltree_operator' => 'Slev\LtreeExtensionBundle\DqlFunction\LtreeOperatorFunction'
+                    ]
+                ]
+            ]
+        ];
+
+        $container->prependExtensionConfig('doctrine', $dbalConfig);
+    }
+
+
     /**
      * {@inheritdoc}
      */
